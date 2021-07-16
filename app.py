f384b21aa745bdc62f82579282704bdfc4f6b23f
Sergey Torotska <73390744+SergeyTorotska@users.noreply.github.com>
2021-07-16 17:14:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,3 @@ app = Flask(__name__)
 def hello():
     return "Hello, World, version 1.0.0 !"
 
-import logging
-from opencensus.ext.azure.log_exporter import AzureLogHandler
-
-logger = logging.getLogger(__name__)
-
-# TODO: replace the all-zero GUID with your instrumentation key.
-logger.addHandler(AzureLogHandler(
-    connection_string='InstrumentationKey=5c47d0c6-2ae8-4e72-ae9d-2e1cee2be5e1')
-)
